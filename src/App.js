c6d0c8a0c7dfb22626d231f0b3b6627380216b99
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-26 17:25:45 +0530

    Stories logic added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Button } from './Button';
 import { SearchForm } from './SearchForm';
+import { Stories } from './Stories';
 
 function App() {
   return (
     <>
     <SearchForm />
     <Button />
+    <Stories />
     </>
   );
 }
